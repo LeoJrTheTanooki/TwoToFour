fd210b89272c5a9f7ed2bd47ee3b6038c96cf1f3
LeoJrTheTanooki <aceraccoon@proton.me>
2024-01-09 14:10:26 -0800

    Updated NumComparer According to given README

--- Services/NumComparer/NumComparerService.cs
+++ Services/NumComparer/NumComparerService.cs
@@ -3,13 +3,42 @@ public class NumComparerService : INumComparerService
 {
     public string CompareNums(int numOne, int numTwo)
     {
+        string part1;
+        string part2;
         if (numOne < numTwo)
         {
-            return $"{numOne} < {numTwo}";
+            part1 = $"{numOne} is less than {numTwo}";
+        }
+        else if (numOne > numTwo)
+        {
+            part1 = $"{numOne} is greater than {numTwo}";
+        }
+        else if (numOne == numTwo)
+        {
+            part1 = $"{numOne} is equal to {numTwo}";
         }
         else
         {
-            return $"{numOne} > {numTwo}";
+            return "Either you got here through some weird glitch or this thing is broken";
         }
+
+        if (numTwo < numOne)
+        {
+            part2 = $"{numTwo} is less than {numOne}";
+        }
+        else if (numTwo > numOne)
+        {
+            part2 = $"{numTwo} is greater than {numOne}";
+        }
+        else if (numTwo == numOne)
+        {
+            part2 = $"{numTwo} is equal to {numOne}";
+        }
+        else
+        {
+            return "Either you got here through some weird glitch or this thing is broken";
+        }
+        return $"{part1}, and {part2}.";
+
     }
 }
